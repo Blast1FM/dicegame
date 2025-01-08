603a085fc109b4d9d7a4962cfca2cee5864b0fd6
zhalnin-jr <grigoriyzhalnin@gmail.com>
2025-01-08 16:56:51 +0300

    Renamed Folder

--- DiceGame.Client/Program.cs
+++ DiceGame.Client/Program.cs
@@ -1,8 +1,21 @@
-﻿namespace DiceGame.Client;
-class Program
+﻿using Avalonia;
+using System;
+
+namespace BakDiceClient;
+
+sealed class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 }
